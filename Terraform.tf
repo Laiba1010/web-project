591c08cf0f5cd5e027f252cf62203908b7ee6385
Laiba Iqbal <101982261+Laiba1010@users.noreply.github.com>
2023-05-17 00:14:03 +0500

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -1,27 +1,81 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
-#resource "aws_s3_bucket" "my_bucket" {
- # bucket = "bucket-web-project-1"
-  #acl    = "private"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-bucket-web"
+  acl    = "private"
 
   versioning {
     enabled = true
   }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
 
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "RestrictAccess",
+      "Sid": "AllowCloudFrontAccess",
       "Effect": "Deny",
       "Principal": "*",
       "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::web-project/*"
+      "Resource": "arn:aws:s3:::my-bucket-web/*",
+     
+        }
+      }
     }
   ]
 }
 EOF
+}
+
+resource "aws_s3_bucket_object" "cache_control" {
+  bucket = aws_s3_bucket.my_bucket.id
+  key    = "index.html"
+  source = "index.html"
+
+  cache_control = "max-age=3600"
+}
+
+resource "aws_cloudfront_distribution" "my_distribution" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "My CloudFront Distribution"
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.my_bucket.website_endpoint
+    origin_id   = aws_s3_bucket.my_bucket.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.my_bucket.id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
