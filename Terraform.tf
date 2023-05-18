d97af282d833ba2505df2801472a30d301839ce8
Laiba Iqbal <101982261+Laiba1010@users.noreply.github.com>
2023-05-19 00:52:17 +0500

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -5,12 +5,11 @@ provider "aws" {
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "dev-laiba-wania-bucket-5"
   acl    = "private"
-}
 
-resource "aws_s3_bucket_website_configuration" "my_bucket_website" {
-  bucket = aws_s3_bucket.my_bucket.id
-  index_document = "index.html"
-  error_document = "error.html"
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -25,7 +24,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       "Effect": "Deny",
       "Principal": "*",
       "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::dev-laiba-wania-bucket-4/*",
+      "Resource": "arn:aws:s3:::dev-laiba-wania-bucket-5/*",
       "Condition": {
         "StringNotEquals": {
           "aws:Referer": "https://${aws_cloudfront_distribution.my_distribution.domain_name}/*"
@@ -76,9 +75,10 @@ resource "aws_cloudfront_distribution" "my_distribution" {
     cloudfront_default_certificate = true
   }
 
-  origin {
+ origin {
     domain_name = aws_s3_bucket.my_bucket.bucket_regional_domain_name
     origin_id   = aws_s3_bucket.my_bucket.id
+  
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.my_oai.cloudfront_access_identity_path
     }
